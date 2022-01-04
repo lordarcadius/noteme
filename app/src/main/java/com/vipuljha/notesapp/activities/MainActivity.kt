b596bbb9afb7052700ee5423694bf9e48139206b
Vipul Jha <vipuljha08@gmail.com>
2022-01-05 00:00:01 +0530

    Initial commit with just a splash screen using new SplashScreen API
    Signed-off-by: Vipul Jha <vipuljha08@gmail.com>

---
+++ app/src/main/java/com/vipuljha/notesapp/activities/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.vipuljha.notesapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.vipuljha.notesapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+        setContentView(R.layout.activity_main)
+
+    }
+}
